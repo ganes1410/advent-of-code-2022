2a1740bc5a9935889acc03c379566275e0cd3325
Ganesh R <ganesramesh110@gmail.com>
2022-12-07 23:42:14 +0530

    move to utils

--- day7/first.ts
+++ day7/first.ts
@@ -1,46 +1,4 @@
-const file = await Deno.readTextFile("./input.txt");
-// const file = await Deno.readTextFile("./testInput.txt");
-const commandsAndOutputs = file.split("\n");
-
-const ROOT_DIR = "/";
-const directoryList = [ROOT_DIR];
-const directorySizes: Record<string, number> = { [ROOT_DIR]: 0 };
-
-for (let index = 1; index < commandsAndOutputs.length; index++) {
-  const currentCommand = commandsAndOutputs[index];
-
-  if (currentCommand?.startsWith("$")) {
-    const command = currentCommand.replace("$", "").trim();
-
-    if (command === "ls") continue;
-
-    if (command.startsWith("cd")) {
-      const [_, dirName] = command.split(" ");
-
-      if (dirName === "..") {
-        directoryList.pop();
-      } else {
-        directoryList.push(dirName);
-      }
-    }
-  } else {
-    if (currentCommand.startsWith("dir")) {
-      const [, dirName] = currentCommand.split(" ");
-      const fullPath = [...directoryList, dirName].join("");
-
-      directorySizes[fullPath] = 0;
-    } else {
-      const [fileSize, _] = currentCommand.split(" ");
-
-      // Add fileSize all directories in array
-      // compute full path because of same name issue
-      directoryList.forEach((_, idx) => {
-        const fullPath = directoryList.slice(0, idx + 1).join("");
-        directorySizes[fullPath] += Number(fileSize);
-      });
-    }
-  }
-}
+import { directorySizes } from "./utils.ts";
 
 const total = Object.values(directorySizes).reduce((acc, curr) => {
   return curr <= 100000 ? acc + curr : acc;
---
+++ day7/utils.ts
@@ -0,0 +1,45 @@
+const file = await Deno.readTextFile("./input.txt");
+// const file = await Deno.readTextFile("./testInput.txt");
+const commandsAndOutputs = file.split("\n");
+
+const ROOT_DIR = "/";
+const directoryList = [ROOT_DIR];
+const directorySizes: Record<string, number> = { [ROOT_DIR]: 0 };
+
+for (let index = 1; index < commandsAndOutputs.length; index++) {
+  const currentCommand = commandsAndOutputs[index];
+
+  if (currentCommand?.startsWith("$")) {
+    const command = currentCommand.replace("$", "").trim();
+
+    if (command === "ls") continue;
+
+    if (command.startsWith("cd")) {
+      const [_, dirName] = command.split(" ");
+
+      if (dirName === "..") {
+        directoryList.pop();
+      } else {
+        directoryList.push(dirName);
+      }
+    }
+  } else {
+    if (currentCommand.startsWith("dir")) {
+      const [, dirName] = currentCommand.split(" ");
+      const fullPath = [...directoryList, dirName].join("");
+
+      directorySizes[fullPath] = 0;
+    } else {
+      const [fileSize, _] = currentCommand.split(" ");
+
+      // Add fileSize all directories in array
+      // compute full path because of same name issue
+      directoryList.forEach((_, idx) => {
+        const fullPath = directoryList.slice(0, idx + 1).join("");
+        directorySizes[fullPath] += Number(fileSize);
+      });
+    }
+  }
+}
+
+export { directorySizes, ROOT_DIR };
