c2bff00ac0b04163e6bcb7a34d9cdc7a59f4a276
Ganesh R <ganesramesh110@gmail.com>
2022-12-08 22:05:51 +0530

    Change File

--- day8/utils.ts
+++ day8/utils.ts
@@ -1,5 +1,5 @@
-const file = await Deno.readTextFile("./testInput.txt");
-// const file = await Deno.readTextFile("./input.txt");
+// const file = await Deno.readTextFile("./testInput.txt");
+const file = await Deno.readTextFile("./input.txt");
 const treeMapsList = file.split("\n");
 const treeMaps = treeMapsList.map((tree) => Array.from(tree).map(Number));
 
