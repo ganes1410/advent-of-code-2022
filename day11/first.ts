2d8f048daa07043590ee252690c7a68333b439de
Ganesh R <ganesramesh110@gmail.com>
2022-12-13 17:03:26 +0530

    complete day11 problem1

--- day11/first.ts
+++ day11/first.ts
@@ -1,6 +1,7 @@
 const notes = Deno.readTextFileSync("./testInput.txt").split("\n\n");
+// const notes = Deno.readTextFileSync("./input.txt").split("\n\n");
 
-let TOTAL_ROUNDS = 1;
+let TOTAL_ROUNDS = 20;
 
 type IMonkey = {
   name: string;
@@ -28,6 +29,43 @@ class Monkey {
     this.negativeCaseMonkeyname = params.negativeCaseMonkeyname;
     this.operations = params.operations;
     this.inspectionCount = 0;
+  }
+
+  inspect(item: number) {
+    const operation = this.operations.split(" ").slice(1);
+    let totalWorryLevel = 0;
+
+    this.items = this.items.filter((item) => item !== item);
+
+    if (operation[1] === "old") {
+      totalWorryLevel = Math.floor((item * item) / 3);
+    } else {
+      switch (operation[0]) {
+        case "+":
+          totalWorryLevel = Math.floor((item + Number(operation[1])) / 3);
+          break;
+        case "*":
+          totalWorryLevel = Math.floor((item * Number(operation[1])) / 3);
+          break;
+      }
+    }
+    this.divide(totalWorryLevel);
+    this.inspectionCount += 1;
+  }
+
+  divide(totalWorryLevel: number) {
+    const isDivisble = totalWorryLevel % this.divisbleBy === 0;
+    const monkeyToThrow = monkeys.find((monkey) => {
+      if (isDivisble) {
+        return monkey.name === this.positiveCaseMonkeyName;
+      } else {
+        return monkey.name === this.negativeCaseMonkeyname;
+      }
+    });
+
+    if (monkeyToThrow) {
+      monkeyToThrow.items.push(totalWorryLevel);
+    }
   }
 }
 
@@ -61,7 +99,23 @@ for (let index = 0; index < notes.length; index++) {
 while (TOTAL_ROUNDS) {
   for (let index = 0; index < monkeys.length; index++) {
     const monkey = monkeys[index];
-    console.log({ monkey });
+    const monkeyItems = monkey.items;
+    for (let itemIndex = 0; itemIndex < monkeyItems.length; itemIndex++) {
+      const item = monkeyItems[itemIndex];
+      monkey.inspect(item);
+    }
   }
   TOTAL_ROUNDS -= 1;
 }
+
+const top2InspectionCount = monkeys
+  .sort(
+    (monkeya, monekeyb) => monekeyb.inspectionCount - monkeya.inspectionCount
+  )
+  .slice(0, 2);
+
+const monkeyBusiness =
+  top2InspectionCount[0].inspectionCount *
+  top2InspectionCount[1].inspectionCount;
+
+console.log({ monkeyBusiness });
